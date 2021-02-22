4eb43dcef4836f98a494f6c5eca49424d07695d1
samuelarogbonlo <sbayo971@gmail.com>
2021-02-22 01:10:04 +0100

    uodated all files

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import pandas as pd 
 
-#Basic day granularity of .csv file (day)
+#Basic granularity of .csv file (day)
 
-dfg = pd.read_csv('Henry_Hub_Natural_Gas_Spot_Price.csv', skiprows=4)
-dfg.index = pd.to_datetime(dfg["Day"],format='%m/%d/%Y')
-dfg.to_csv('gas-details_day.csv', index=False) 
+dfg = pd.read_csv('Henry_Hub_Natural_Gas_Spot_Price.csv',
+                 index_col='Day',parse_dates=True,infer_datetime_format=False,skiprows=4)
+dfg.sort_index(inplace=True)
+dfg.to_csv('gas-details_day.csv', index=True) 
 
 #Other granularities and sections of the .csv file (month)
-
 dfg_month = dfg['Henry Hub Natural Gas Spot Price Dollars per Million Btu'].resample('M').sum()
 df = pd.DataFrame(dfg_month, index=dfg_month.index.strftime("%m"))
-df.to_csv('gas-details_month.csv', index=True)
+df.to_csv('gas-details_month.csv', index=True) 
+
 
 #Other granularities and sections of the .csv file (year)
 dfg_year = dfg['Henry Hub Natural Gas Spot Price Dollars per Million Btu'].resample('Y').sum()
